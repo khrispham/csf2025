12d9b52878d9ca92af798a90eb35e79e6d3b62f4
Khris Pham <khrispham@gmail.com>
2025-04-16 23:04:16 -0400

    add sender loop

--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -46,6 +46,40 @@ int main(int argc, char **argv) {
 
   // TODO: loop reading commands from user, sending messages to
   //       server as appropriate
+  std::string input;
+  int input_size;
+  std::string msgtag;
+  std::string msgdata;
+  std::string msg_encoded;
+
+  while(1){
+    //read user input
+    if (!std::getline(std::cin, input)) {
+      break;  
+    }
+    //input = trim(input);
+    input_size = input.size();
+
+    std::string delimiter = " ";
+    size_t pos = input.find(delimiter);
+
+    //if input has no payload
+    if (pos == std::string::npos) {
+      msgtag = input.substr(1,input_size);
+      msgdata = "";
+    } else {
+    //input has payload
+      msgtag = input.substr(1,pos);
+      msgdata = input.substr(pos+1, input_size-pos-1);
+    }
+
+    //create Message to send
+    Message msg(msgtag, msgdata);
+    msg_encoded = msg.encode();
+    //std::cout << msg_encoded;
+
+    rio_writen(clientfd, msg_encoded.c_str(), input_size);
+  }
 
   return 0;
 }
